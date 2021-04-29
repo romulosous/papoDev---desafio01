96e532e66150e54cb9547e8a16ff399d6b1af5bf
Romulo Carvalho <romulocarvalho081@gmail.com>
2021-04-29 14:06:58 -0300

    add modal

--- index.js
+++ index.js
@@ -50,3 +50,13 @@ function changeBackground () {
     actionsContainer.classList.remove('dark')
   }
 }
+
+function modal () {
+  const modalOverlay = document.querySelector('.modal-overlay')
+
+  document.querySelector('.close-modal').addEventListener('click', function () {
+    modalOverlay.classList.remove('active')
+  })
+}
+
+modal()
